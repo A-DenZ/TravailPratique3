ce89d55047ab4431047865dd8450053c8e7dd8bb
Jay Carle Brodeur <jay.carle.brodeur.01@edu.cegeptr.qc.ca>
2022-11-23 08:47:27 -0500

    MidProject

---
+++ Tp3/Tp3/Projet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp3
+{
+    internal class Projet
+    {
+        string numero;
+        string date;
+        int budget;
+        string description;
+        string employe;
+
+        public string Numero { get => numero; set => numero = value; }
+        public string Date { get => date; set => date = value; }
+        public int Budget { get => budget; set => budget = value; }
+        public string Description { get => description; set => description = value; }
+        public string Employe { get => employe; set => employe = value; }
+    }
+}
